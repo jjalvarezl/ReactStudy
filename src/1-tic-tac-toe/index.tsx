c6df77ad2cc5ffec29861b52fdfe3f2b32f6127d
Jhon Alvarez <jalvarez@blackboard.com>
2022-07-29 19:02:21 -0500

    Completting the tic-tac-toe game.

--- src/1-tic-tac-toe/index.tsx
+++ src/1-tic-tac-toe/index.tsx
@@ -2,20 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./index.css"
 
-/**
- * The tic tac toe game has the intention to understand step by step hoy react behaves.
- * Here you can find a short rationale of changes around the code:
- * 
- * 1. The initial code has multiple empty and non functional components to complete through it's tutorial.
- * 2. The this.renderSquare(n) functions are passing an id of the square to being draw by using props.
- * 3. Props have been replaced with state to draw an "X" inside each square. 
- *    The this.renderSquare function is now without any parameter.
- * 4. The board rendering is now depending on the board dimension. 
- *    The "key" reserverd word must be filled inside the div of the row as well as the Square component for react standards.
- *    More information at https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
- * 5. We passed the square state to be inside the board component and not belonging each squate itself by duplicating it's state.
- */
-
 type SquareProps = {
   value: string | null,
   onClick: () => void
@@ -27,36 +13,82 @@ interface BoardProps {
 
 interface BoardState {
   squares: string[];
+  xIsNext: boolean;
+  winner: string | null;
+  winnerLines: number[][];
 }
 
-class Square extends Component <SquareProps> {
-  render() {
-    return (
-      <button className="square" onClick={ this.props.onClick } >
-        { this.props.value }
-      </button>
-    );
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function calculateWinnerLines(dimension: number): number[][]{
+  let winnerLines: number[][] = [];
+  let firstDiagonal: number[] = [];
+  let secondDiagonal: number[] = [];
+
+  for (let i: number = 0; i<dimension; i++){
+    let verticalLine: number[] = [];
+    let horizontalLine: number[] = [];
+    for (let j: number = 0; j<dimension; j++){
+      if (i === j) firstDiagonal.push(((dimension-1) * i)+i+j);
+      if (i+j === (dimension-1)) secondDiagonal.push( (dimension -1) * (i+1) );
+      verticalLine.push( i + (j*dimension) );
+      horizontalLine.push( j + (dimension*i));
+    }
+    winnerLines.push(verticalLine);
+    winnerLines.push(horizontalLine);
   }
+
+  winnerLines.push(firstDiagonal);
+  winnerLines.push(secondDiagonal);
+
+  return winnerLines;
+}
+
+function calculateWinner(winnerLines: number[][], squares: string[]): string | null {
+  let winner: string | null = null;
+  for (let i: number = 0; i<winnerLines.length && !winner; i++){
+    winner = squares[winnerLines[i][0]];
+    for (let j: number = 0; j<winnerLines[i].length && (winner = winner === squares[winnerLines[i][j]] ? winner : null); j++){}
+  }
+  return winner;
 }
 
 class Board extends Component<BoardProps, BoardState> {
   constructor(props: BoardProps) {
     super(props);
     this.state = {
-      squares: Array(this.props.dimension).fill(null),
+      squares: Array( Math.pow( this.props.dimension, 2 ) ).fill(null),
+      xIsNext: true,
+      winner: null,
+      winnerLines: calculateWinnerLines(this.props.dimension)
     }
   }
 
   handleClick(i: number) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if (!this.state.winner) {
+      const squares = this.state.squares.slice();
+      const fillSquare = !squares[i];
+      squares[i] = this.state.xIsNext && fillSquare ? 'X' : 'O';
+      if (fillSquare) {
+        this.setState({ 
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+          winner: calculateWinner(this.state.winnerLines, squares)
+        });
+      }
+    }
   }
 
   renderSquare(i: number) {
     return (
-      <Square 
-        key={"square-"+i.toString()}
+      <Square
+        key={"square-" + i.toString()}
         value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
       />
@@ -64,14 +96,19 @@ class Board extends Component<BoardProps, BoardState> {
   }
 
   render() {
-    const status = 'Next player: X';
+    let status: string;
+    if (this.state.winner) {
+      status = "Winner: "+ this.state.winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     let renderBoard: JSX.Element[] = [];
     for (let i: number = 0; i < this.props.dimension; i++) {
       let renderRow: JSX.Element[] = []
-      for (let j: number = 0; j < this.props.dimension; j++){
-        renderRow.push(this.renderSquare(i*this.props.dimension+j));
+      for (let j: number = 0; j < this.props.dimension; j++) {
+        renderRow.push(this.renderSquare(i * this.props.dimension + j));
       }
-      renderBoard.push(<div key={"board-row-"+i.toString()} className="board-row">{renderRow}</div>);
+      renderBoard.push(<div key={"board-row-" + i.toString()} className="board-row">{renderRow}</div>);
     }
 
     return (
@@ -88,7 +125,7 @@ class Game extends Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board dimension={3}/>
+          <Board dimension={5} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
@@ -104,7 +141,7 @@ function TicTacToe() {
   return (
     <>
       <main>
-        <Game/>
+        <Game />
       </main>
       <nav>
         <Link to={`/`}>Home</Link>
