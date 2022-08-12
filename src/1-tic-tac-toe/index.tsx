75f2f42c75621d865b4760c132ab76d10b35a3b8
Jhon Alvarez <jalvarez@blackboard.com>
2022-08-12 16:08:21 -0500

    Finishing the game

--- src/1-tic-tac-toe/index.tsx
+++ src/1-tic-tac-toe/index.tsx
@@ -2,29 +2,35 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./index.css"
 
+//------------------ PROPS ----------------------
+
 type SquareProps = {
   value: string | null,
   onClick: () => void
 }
 
 interface BoardProps {
-  dimension: number
+  dimension: number,
+  squares: string[];
+  onClick: (i: number) => void
 }
 
-interface BoardState {
-  squares: string[];
-  xIsNext: boolean;
+interface GameProps {
+  dimension: number;
+}
+
+interface GameState {
+  dimension: number;
+  history: {
+    squares: string[]
+  }[],
   winner: string | null;
   winnerLines: number[][];
+  xIsNext: boolean;
+  stepNumber: number;
 }
 
-function Square(props: SquareProps) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+//------------------ AUX FUNCTIONS ----------------------
 
 function calculateWinnerLines(dimension: number): number[][]{
   let winnerLines: number[][] = [];
@@ -59,49 +65,20 @@ function calculateWinner(winnerLines: number[][], squares: string[]): string | n
   return winner;
 }
 
-class Board extends Component<BoardProps, BoardState> {
-  constructor(props: BoardProps) {
-    super(props);
-    this.state = {
-      squares: Array( Math.pow( this.props.dimension, 2 ) ).fill(null),
-      xIsNext: true,
-      winner: null,
-      winnerLines: calculateWinnerLines(this.props.dimension)
-    }
-  }
-
-  handleClick(i: number) {
-    if (!this.state.winner) {
-      const squares = this.state.squares.slice();
-      const fillSquare = !squares[i];
-      squares[i] = this.state.xIsNext && fillSquare ? 'X' : 'O';
-      if (fillSquare) {
-        this.setState({ 
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
-          winner: calculateWinner(this.state.winnerLines, squares)
-        });
-      }
-    }
-  }
-
+//------------------ BOARD COMPONENT ----------------------
+class Board extends Component<BoardProps> {
   renderSquare(i: number) {
     return (
       <Square
         key={"square-" + i.toString()}
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    let status: string;
-    if (this.state.winner) {
-      status = "Winner: "+ this.state.winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+    
     let renderBoard: JSX.Element[] = [];
     for (let i: number = 0; i < this.props.dimension; i++) {
       let renderRow: JSX.Element[] = []
@@ -112,26 +89,138 @@ class Board extends Component<BoardProps, BoardState> {
     }
 
     return (
-      <div>
-        <div className="status">{status}</div>
+      <div className="renderStatus">
         {renderBoard}
       </div>
     );
   }
 }
 
-class Game extends Component {
+//------------------ SQUARE COMPONENT ----------------------
+
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+//------------------ GAME COMPONENT ----------------------
+class Game extends Component<GameProps, GameState> {
+  constructor(props: GameProps) {
+    super(props);
+    this.state = {
+      dimension: props.dimension,
+      history: [{
+        squares: Array(Math.pow( this.props.dimension, 2 )).fill(null),
+      }],
+      winner: null,
+      winnerLines: calculateWinnerLines(props.dimension),
+      stepNumber:0,
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i: number) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (!this.state.winner) {
+      const fillSquare = !squares[i];
+      squares[i] = this.state.xIsNext && fillSquare ? 'X' : 'O';
+      if (fillSquare) {
+        this.setState({ 
+          history: history.concat([{
+            squares: squares,
+          }]),
+          xIsNext: !this.state.xIsNext,
+          winnerLines: calculateWinnerLines(this.state.dimension),
+          winner: calculateWinner(this.state.winnerLines, squares),
+          stepNumber: history.length,
+        });
+      }
+    }
+  }
+
+  handleDimmensionChange(newDimension: number) {
+    if (newDimension !== this.state.dimension) {
+      this.setState({
+        dimension: newDimension,
+        winner: null,
+        xIsNext: true,
+        winnerLines: calculateWinnerLines(newDimension),
+        stepNumber:0,
+        history: [{
+          squares: Array(Math.pow( newDimension, 2 )).fill(null),
+        }],
+      });
+    }
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
+    let status: string;
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+
+    const moves = this.state.history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let isFullfilledBoard = true;
+    for (let i = 0; i<current.squares.length && isFullfilledBoard; i++ ){
+      isFullfilledBoard = isFullfilledBoard && current.squares[i] != null;
+    }
+
+    if (this.state.winner) {
+      status = "Winner: "+ this.state.winner;
+    } else if (!this.state.winner && isFullfilledBoard) {
+      status = "No winner!!!";
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    
     return (
+      <>
+      <div className="game-configuration">
+        <label>
+          Dimension:
+          <input type="number" value={this.state.dimension} 
+            onChange={(event) => {this.handleDimmensionChange(+event.target.value)}}
+            min="2" max="10"
+          />
+        </label>
+
+      </div>
+      <hr />
       <div className="game">
         <div className="game-board">
-          <Board dimension={5} />
+          <Board 
+            dimension={this.state.dimension} 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
+      </>
     );
   }
 }
@@ -141,7 +230,7 @@ function TicTacToe() {
   return (
     <>
       <main>
-        <Game />
+        <Game dimension={3}/>
       </main>
       <nav>
         <Link to={`/`}>Home</Link>
